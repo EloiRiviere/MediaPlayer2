31f0d070d9f40a2535b7c371036a99f68c31053c
Eloi <eloi.riviere@hotmail.com>
2018-12-05 10:54:50 +0100

    nettoyage du main

--- MediaPlayer2/src/launch/Main.java
+++ MediaPlayer2/src/launch/Main.java
@@ -6,24 +6,13 @@
 package launch;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Menu;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
 
 /**
  *
