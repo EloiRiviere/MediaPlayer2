263a187d8ce58dd5141b6db32ef53d0807f79c1c
lesaugues <leo.saugues@etu.uca.fr>
2018-12-05 11:12:43 +0100

    class musique

---
+++ MediaPlayer2/src/donnée/Musique.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package donnée;
+
+/**
+ *
+ * @author leo
+ */
+public class Musique 
+{
+    private String titre;
+    private String artiste;
+    private String album;
+    private float duree;
+    
+
+    
+}
