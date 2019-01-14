4de2f7f3b3f8514ed7cfb6ecfd2c8a877978da2c
elriviere2 <eloi.riviere@etu.uca.fr>
2019-01-14 13:32:48 +0100

    vue update

--- src/launch/Main.java
+++ src/launch/Main.java
@@ -1,25 +1,10 @@
 package launch;
 
 import javafx.application.Application;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.scene.control.ListView;
-
-import modeles.*;
-import test.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -33,7 +18,7 @@ public class Main extends Application
         primaryStage.setTitle("MediaPlayer");
         
         BorderPane root = FXMLLoader.load(this.getClass().getClassLoader().getResource("ressources/accueil.fxml"));
-        root.getStylesheets().add(getClass().getResource("/ressources/style.css").toExternalForm());
+        root.getStylesheets().add(getClass().getResource("/ressources/style2.css").toExternalForm());
 
         primaryStage.setScene(new Scene(root,1200,700));
         primaryStage.show();
